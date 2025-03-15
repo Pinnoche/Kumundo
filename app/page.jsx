08a9290bf6340332adcd1fa94fdcf7da50ecd97a
pinnoche <ptime552@gmail.com>
2025-03-16 00:37:52 +0100

    fix ssr window call

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,11 @@
 "use client"
 
-import SmoothScroll from 'smooth-scroll';
-import { useEffect } from "react";
+
 import Section1 from "./Sections/Section1";
 import Section2 from "./Sections/Section2";
 import Section3 from "./Sections/Section3";
 
 export default function Home() {
-  useEffect(() => {
-    new SmoothScroll('a[href*="#"]', {
-      speed: 800, 
-    });
-  }, []);
   return (
     <main className="min-h-screen pt-16 text-white scroll-smooth">
       <Section1 />
